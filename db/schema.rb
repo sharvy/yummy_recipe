ae03cae6072eac79779321a9099b1294cd4f9597
Sharvy Ahmed <sharvy2008@gmail.com>
2015-01-14 05:51:45 +0600

    Added models.

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150113234311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "food_preferences", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "food_types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# food_types = %w(Curry Dessert Sides Breakfast)
+# food_types.each{ |type| FoodType.where(name: type).create }
+
+food_preferences = %w(Vegetarian Vegan Meat Dairy)
+food_preferences.each{ |d| FoodPreference.where(name: d).first_or_create }
+
+cuisines = %w(Italian Mexican Indian Chinese)
+cuisines.each{ |d| Cuisine.where(name: d).first_or_create }
