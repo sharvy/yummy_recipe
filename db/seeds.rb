16adcc302ffd403fe9bc8015bd5a8d2b4452bd23
Sharvy Ahmed <sharvy2008@gmail.com>
2015-01-14 19:38:42 +0600

    Added some models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-# food_types = %w(Curry Dessert Sides Breakfast)
-# food_types.each{ |type| FoodType.where(name: type).create }
+food_types = %w(Curry Dessert Sides Breakfast)
+food_types.each{ |type| FoodType.where(name: type).first_or_create }
 
 food_preferences = %w(Vegetarian Vegan Meat Dairy)
 food_preferences.each{ |d| FoodPreference.where(name: d).first_or_create }
